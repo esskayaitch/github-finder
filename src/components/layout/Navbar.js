090e3aa77b69f6bedf1f2226924165a75232aa81
esskayaitch <simonhillskh@gmail.com>
2021-10-26 22:13:40 +1100

    Initial - just inserted github icon on navbar.

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <i className="fab fa-github" /> nav bar
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
