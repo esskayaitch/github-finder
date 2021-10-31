2cf1bbdbc9b3a26e26152ef6ea4a1dfc06d2c9b1
esskayaitch <simonhillskh@gmail.com>
2021-10-31 22:03:08 +1100

    V2. Initial plus target=_blank in RepoItem.

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -5,7 +5,9 @@ const RepoItem = ({ repo }) => {
   return (
     <div className="card">
       <h3>
-        <a href={repo.html_url}>{repo.name}</a>
+        <a href={repo.html_url} target="_blank" rel="noreferrer noopener">
+          {repo.name}
+        </a>
       </h3>
     </div>
   );
